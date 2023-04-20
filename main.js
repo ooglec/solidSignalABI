6711f9f214a5aa7c229dae6d1d997264fcbaa255
ooglec <delalie@reallygreattech.com>
2023-04-20 15:22:20 +0100

    insufficinet check

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let provider;
 let signer;
 let price;
-const solidAddress = "0x3de69A2Ae60838D4a854647366012723D7a6498F";
+const solidAddress = "0xcaE8A39DC4401d0b82f7445262dBe1B5dd75CA9B";
 const usdcAddress = "0x6b52834DDDa183E4C01d20f1421412035c66Da54";
 const chainName = 'Arbitrum Goerli';
 let solidSpendAllowance = 0;
