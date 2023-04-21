72058231b5869edb940678f08b0dd99d2f6c706c
ooglec <delalie@reallygreattech.com>
2023-04-21 08:58:55 +0100

    toast notification module

--- main.js
+++ main.js
@@ -357,6 +357,23 @@ function toast(text, bg) {
     }).showToast();
 }
 
+function createToast(type, icon, title, text) {
+    let newToast = document.createElement('div');
+    newToast.innerHTML = `
+        <div class="toast ${type}">
+            <i class="${icon}"></i>
+            <div class="content">
+                <div class="title">${title}</div>
+                <span>${text}</span>
+            </div>
+            <i class="fa-solid fa-xmark" onclick="(this.parentElement).remove()"></i>
+        </div>`;
+    notifications.appendChild(newToast);
+    newToast.timeOut = setTimeout(
+        () => newToast.remove(), 5000
+    )
+}
+
 function isWrongNetwork() {
     if (chains.includes(provider.chainId) || chains.includes(window.ethereum.chainId)) {
         return false
